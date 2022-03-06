dc58b861465b40054b327a31bdd40343060e0c80
Sergei_Zhukov <zhukovmail@gmail.com>
2022-03-06 15:07:05 -0500

    map GeoJSON Points finish

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -8,25 +8,70 @@ console.log("working");
 // let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+// let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng)
+    .bindPopup("<h3>" + "Airport Code: " +feature.properties.faa + "</h3> <hr> <h3>" + "Airport Name: " +feature.properties.name + "</h3>");
+  }
+
+}).addTo(map);
+
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city+ ", " + feature.properties.country+ "</h3>");
+//   }
+
+// }).addTo(map);
 
 // Coordinates for each point to be used in the line.
 // Coordinates for each point to be used in the polyline.
-let line = [
-    [33.9416, -118.4085],
-    [30.1975, -97.6664],
-    [43.6777, -79.6248],
-    [28.64179, -81.3041]
-  ];
+// let line = [
+//     [33.9416, -118.4085],
+//     [30.1975, -97.6664],
+//     [43.6777, -79.6248],
+//     [28.64179, -81.3041]
+//   ];
 
 // Create a polyline using the line coordinates and make the line yellow.
-L.polyline(line, {
-    color: "red",
-    dashArray: '5, 5', 
-    dashOffset: '15',
-    weight: 4,
-    opacity: 0.5 
- }).addTo(map);
+// L.polyline(line, {
+//     color: "red",
+//     dashArray: '5, 5', 
+//     dashOffset: '15',
+//     weight: 4,
+//     opacity: 0.5 
+//  }).addTo(map);
 
 // // Circle Marker using circleMarker
 // L.circleMarker([34.0522, -118.2437], {
